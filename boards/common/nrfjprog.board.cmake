15bae6f2de45ab4e6d1e5be79865de59ec72ec87
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-11-23 13:19:23 -0800

    boards: nordic: reduce nrfjprog runner boilerplate
    Every board.cmake file for an nRF SoC target is repeating boilerplate
    needed for the nrfjprog runner's --nrf-family argument.
    
    The information we need to decide the --nrf-family is already
    available in various CONFIG_SOC_xxx symbols, so just get it from there.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- boards/common/nrfjprog.board.cmake
+++ boards/common/nrfjprog.board.cmake
@@ -1,4 +1,29 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(NRFJPROG_COMMON_RUNNER_ARGS)
+
+if(CONFIG_SOC_SERIES_NRF51X)
+  list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--nrf-family=NRF51")
+elseif(CONFIG_SOC_SERIES_NRF52X)
+  list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--nrf-family=NRF52")
+elseif(CONFIG_SOC_SERIES_NRF53X)
+  list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--nrf-family=NRF53")
+
+  # As of now, we only have one SoC to support, so we get the
+  # --coprocessor value by matching against nRF5340 specific Kconfig
+  # symbols.
+  if(CONFIG_SOC_NRF5340_CPUAPP)
+    list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--tool-opt=--coprocessor CP_APPLICATION")
+  elseif(CONFIG_SOC_NRF5340_CPUNET)
+    list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--tool-opt=--coprocessor CP_NETWORK")
+  else()
+    message(WARNING "Unknown nRF53 SoC; please update nrfjprog.board.cmake")
+  endif()
+elseif(CONFIG_SOC_SERIES_NRF91X)
+  list(APPEND NRFJPROG_COMMON_RUNNER_ARGS "--nrf-family=NRF91")
+else()
+  message(WARNING "Unknown nRF SoC series; please update nrfjprog.board.cmake")
+endif()
+
 board_set_flasher_ifnset(nrfjprog)
-board_finalize_runner_args(nrfjprog) # No default arguments to provide.
+board_finalize_runner_args(nrfjprog ${NRFJPROG_COMMON_RUNNER_ARGS})
